be150b2cea1ad6e48f6c62fb6985ac418add4f07
robothaver <robothaver109@gmail.com>
2024-01-01 22:55:08 +0100

    Improved Update_Configfile

--- Modules/Configfile/Update_Configfile.py
+++ Modules/Configfile/Update_Configfile.py
@@ -3,9 +3,12 @@ import json
 
 class UpdateConfigfile:
     # This class updates the Config.json
-    def __init__(self, key, value):
+    def __init__(self, key, value, index=None):
         with open("Config.json", "r") as jsonFile:
             data = json.load(jsonFile)
-        data[key] = value
+        if index is not None:
+            data[key][index] = value
+        else:
+            data[key] = value
         with open("Config.json", "w") as jsonFile:
             json.dump(data, jsonFile, indent=3)
