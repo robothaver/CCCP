95ca663a2f59231322d651357e7785451fa2228f
robothaver <robothaver109@gmail.com>
2024-01-01 22:56:16 +0100

    Created relative path generator utility

---
+++ Modules/Utilities/Get_Realtive_Path.py
@@ -0,0 +1,15 @@
+from ttkbootstrap.dialogs.dialogs import Messagebox
+import os
+
+
+class GetRelativePath:
+    def __init__(self, path):
+        self.path = path
+        self.results = None
+
+    def get_path(self, start=""):
+        try:
+            self.results = os.path.relpath(start=start, path=self.path)
+        except ValueError:
+            Messagebox.show_error(title="Error", message="Path destination is on a different drive!")
+        return self.results
